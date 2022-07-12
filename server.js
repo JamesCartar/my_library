49d8aa407d59f42576e6fc73bce5985332ff0af3
heinhtet <hein08308@gmail.com>
2022-07-12 17:02:19 +0630

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+const connectDB = require("./db/connect");
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.set("vew engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port http://www.localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
